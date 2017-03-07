634649a95f5e9ab49dabfb55a82f7481eb0bd8f5
at-hoaiphan <phanvanhoaivh4394@gmail.com>
2017-03-07 16:53:38 +0700

    Them textView: tao conflict

---
+++ app/src/main/java/com/example/gio/firstproject/LoginScreen.java
@@ -0,0 +1,17 @@
+package com.example.gio.firstproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Gio on 3/7/2017.
+ */
+
+public class LoginScreen extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+}
