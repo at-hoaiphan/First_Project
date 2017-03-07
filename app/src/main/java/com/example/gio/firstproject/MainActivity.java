7e4f385991710ec14a624fe252a2de4454cda5ec
at-hoaiphan <phanvanhoaivh4394@gmail.com>
2017-03-07 12:02:19 +0700

    first commit

---
+++ app/src/main/java/com/example/gio/firstproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gio.firstproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int i=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
