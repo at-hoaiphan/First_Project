8300f0cb5295770520c53187a23e325af8668a1a
at-hoaiphan <phanvanhoaivh4394@gmail.com>
2017-03-08 16:46:47 +0700

    update branch

--- app/src/main/java/com/example/gio/firstproject/LoginScreen.java
+++ app/src/main/java/com/example/gio/firstproject/LoginScreen.java
@@ -1,17 +1,35 @@
 package com.example.gio.firstproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Gio on 3/7/2017.
  */
 
-public class LoginScreen extends AppCompatActivity{
+public class LoginScreen extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnSignUp;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnSignUp:
+                Intent i = new Intent()
+        }
     }
 }
---
+++ app/src/main/java/com/example/gio/firstproject/RegisterScreen.java
@@ -0,0 +1,19 @@
+package com.example.gio.firstproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Gio on 3/8/2017.
+ */
+
+public class RegisterScreen extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView();
+    }
+}
