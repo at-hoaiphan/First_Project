afffc7e7c18a0bcce5fa6b5e4d4204dab8fb990c
at-hoaiphan <phanvanhoaivh4394@gmail.com>
2017-03-09 18:01:05 +0700

    Connect layouts

---
+++ app/src/main/java/com/example/gio/firstproject/InformationLayout.java
@@ -0,0 +1,16 @@
+package com.example.gio.firstproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Gio on 3/9/2017.
+ */
+
+public class InformationLayout extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inf_adapter_layout);
+    }
+}
